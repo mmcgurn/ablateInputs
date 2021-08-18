6639795a5947a01a6841aad5d9fef0a276eee871
matt mcgurn <matt@mcgurn.dev>
2021-08-18 06:56:10 -0400

    added v6

--- canteraCalculator/inputCalc.py
+++ canteraCalculator/inputCalc.py
@@ -33,7 +33,7 @@ gas.TPY = inletTemperature, inletPressure, inletYi
 inletDensity = gas.density
 
 # add in the blowing conditions
-blowTemperature = 300  # Kelvin
+blowTemperature = 1000  # Kelvin
 blowPressure = 101325  # Pascals
 blowYi = {'CH4': 1.0}
 gas.TPY = blowTemperature, blowPressure, blowYi
@@ -50,9 +50,11 @@ outletDensity = gas.density
 
 # compute the mass coming in
 mass = 0.0;
-inletArea = 0.0254**2
+# inletArea = 0.0254**2 # 3D
+inletArea = 0.0276
 mass += inletDensity * 20.0 * inletArea
-slabArea = 0.00762*sqrt((.036501-0.025)**2 + (.01146- 0.0)**2)
+# slabArea = 0.00762*sqrt((.036501-0.025)**2 + (.01146- 0.0)**2) #3D
+slabArea = sqrt((.036501-0.025)**2 + (.01146- 0.0)**2)
 mass += blowDensity * sqrt(5**2 + 5**2) * slabArea
 
 outletVelocity = mass /(inletArea*outletDensity)
