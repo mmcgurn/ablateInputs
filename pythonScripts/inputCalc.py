d67fe9ed5ad00cba8949592ef2e09c1fffbdea44
Matt McGurn <75279702+mmcgurn@users.noreply.github.com>
2022-10-04 16:29:59 -0400

    added version 4

--- pythonScripts/inputCalc.py
+++ pythonScripts/inputCalc.py
@@ -25,27 +25,20 @@ inletYi = {'O2': 1.0}
 gas.TPY = inletTemperature, inletPressure, inletYi
 inletDensity = gas.density
 
-# add in the blowing conditions
-blowTemperature = 1500  # Kelvin
-blowPressure = 101325  # Pascals
-blowYi = {'CH4': 1.0}
-gas.TPY = blowTemperature, blowPressure, blowYi
-blowDensity = gas.density
-
 # compute the mass coming in
-inletG = 22.19 # kg/m2/s
+gAtFuel = 22.19 # kg/m2/s
+
+# set areas
 inletArea = 0.027686*(0.0127*2)
+fuelArea = 0.0114*(0.00381*2)
 
-inletVel = inletG/inletDensity
-inletMassRate = inletG*inletArea # kg/(m2 s) * m2 = kg/s
+# fuel normal area
+fuelFlowArea = inletArea - fuelArea;
 
-# compute stoic ratio
-o2FuelRatio = 3.989
+# compute G at inlet
+gAtInlet = gAtFuel*fuelFlowArea/inletArea
+inletVel = gAtInlet/inletDensity
 
-# compute the fuelInlet
-fuelRate = inletMassRate/o2FuelRatio # kg/s
-fuelArea = 0.06858*(0.00381*2) #m2
-fuelVelocity = fuelRate/(blowDensity*fuelArea)
-
-print("inletVelocity", inletVel)
-print("fuelVelocity", fuelVelocity)
+print("O2Density", inletDensity)
+print("velocityAtFuel", gAtFuel/inletDensity)
+print("inletVel", inletVel)
